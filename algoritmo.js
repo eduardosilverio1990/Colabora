1f915d23271408232b1df6d4f876a330ebecec74
Eduardo Silverio da Silva <eduardosilverio1990@gmail.com>
2018-04-02 21:59:05 -0400

    6

--- algoritmo.js
+++ algoritmo.js
@@ -2,4 +2,8 @@ window.onload = function() {
 	//0 - Vetores com as informacoes de cores e nomes de regioes a serem utilizadas no mapa e na legenda
 	var cores = ["#7fc97f", "#beaed4", "#fdc086", "#ffff99", "#386cb0"];
 	var regioes = ["Norte", "Nordeste", "Sudeste", "Sul", "Centro Oeste"];
+	
+	//6 - Mapa de Visao Geral (plugin)
+	var osmColorido2 = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png");
+	var mapaVisaoGeral = new L.Control.MiniMap(osmColorido2).addTo(map);
 }
