bb45a050560abcdcf2782f7b70159d2353d512a9
Eduardo Silverio da Silva <eduardosilverio1990@gmail.com>
2018-04-02 22:00:36 -0400

    10

--- algoritmo.js
+++ algoritmo.js
@@ -2,4 +2,9 @@ window.onload = function() {
 	//0 - Vetores com as informacoes de cores e nomes de regioes a serem utilizadas no mapa e na legenda
 	var cores = ["#7fc97f", "#beaed4", "#fdc086", "#ffff99", "#386cb0"];
 	var regioes = ["Norte", "Nordeste", "Sudeste", "Sul", "Centro Oeste"];
+	
+	//10 - Legenda
+	for(var i=0; i<regioes.length; i++) {
+		document.getElementById("legenda").innerHTML += "<caixalegenda style='background-color:" + cores[i] + "'></caixalegenda> <textolegenda>" + regioes[i] + "</textolegenda> <br />";
+	}
 }
