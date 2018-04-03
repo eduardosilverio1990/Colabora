ba8da52f3d911b66080cee25d0080655c97e19ad
Eduardo Silverio da Silva <eduardosilverio1990@gmail.com>
2018-04-02 21:58:40 -0400

    5

--- algoritmo.js
+++ algoritmo.js
@@ -2,4 +2,9 @@ window.onload = function() {
 	//0 - Vetores com as informacoes de cores e nomes de regioes a serem utilizadas no mapa e na legenda
 	var cores = ["#7fc97f", "#beaed4", "#fdc086", "#ffff99", "#386cb0"];
 	var regioes = ["Norte", "Nordeste", "Sudeste", "Sul", "Centro Oeste"];
+	
+	//5 - Funcao para anexar etiquetas com o nome de cada estado
+	function colocarTooltips(feicao, camada) {
+		camada.bindTooltip(feicao.properties.NOME);
+	}
 }
