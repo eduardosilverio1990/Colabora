c5e80f60c3a662fdbab5e47577c615b9c06d3058
Eduardo Silverio da Silva <eduardosilverio1990@gmail.com>
2018-04-02 22:01:43 -0400

    13

--- algoritmo.js
+++ algoritmo.js
@@ -2,4 +2,7 @@ window.onload = function() {
 	//0 - Vetores com as informacoes de cores e nomes de regioes a serem utilizadas no mapa e na legenda
 	var cores = ["#7fc97f", "#beaed4", "#fdc086", "#ffff99", "#386cb0"];
 	var regioes = ["Norte", "Nordeste", "Sudeste", "Sul", "Centro Oeste"];
+	
+	//13 – Medicao de distancias (plugin)
+	var medicaoDistancias = new L.control.polylineMeasure().addTo(map);
 }
