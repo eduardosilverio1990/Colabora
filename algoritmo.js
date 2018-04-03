c8e0e4834777a5022218353e91b8805dba76a9fb
Eduardo Silverio da Silva <eduardosilverio1990@gmail.com>
2018-04-02 21:57:53 -0400

    3

--- algoritmo.js
+++ algoritmo.js
@@ -2,4 +2,10 @@ window.onload = function() {
 	//0 - Vetores com as informacoes de cores e nomes de regioes a serem utilizadas no mapa e na legenda
 	var cores = ["#7fc97f", "#beaed4", "#fdc086", "#ffff99", "#386cb0"];
 	var regioes = ["Norte", "Nordeste", "Sudeste", "Sul", "Centro Oeste"];
+	
+	//3 - Camadas de sobreposiçao
+	var camadaGeojson = L.geoJson(estadosBrasil, {
+		style: obterSimbologia,
+		onEachFeature: colocarTooltips
+	}).addTo(map);
 }
