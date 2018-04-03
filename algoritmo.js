892825804dde7293e53dcd5e5c5f34464a0f9a85
Eduardo Silverio da Silva <eduardosilverio1990@gmail.com>
2018-04-02 21:57:08 -0400

    1

--- algoritmo.js
+++ algoritmo.js
@@ -2,4 +2,14 @@ window.onload = function() {
 	//0 - Vetores com as informacoes de cores e nomes de regioes a serem utilizadas no mapa e na legenda
 	var cores = ["#7fc97f", "#beaed4", "#fdc086", "#ffff99", "#386cb0"];
 	var regioes = ["Norte", "Nordeste", "Sudeste", "Sul", "Centro Oeste"];
+	
+	//1 - Mapa centrado no Brasil
+	var map = L.map("meumapa", {
+		center: [-15, -49],
+		zoom: 4.5,
+		zoomSnap: 0.5,
+		zoomDelta: 0.5,
+		minZoom: 4.5,
+		maxZoom: 8.5
+	});
 }
