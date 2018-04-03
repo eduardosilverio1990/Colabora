bbe69abc5266a507261d4b8328291bf9b18c513a
Eduardo Silverio da Silva <eduardosilverio1990@gmail.com>
2018-04-02 21:57:29 -0400

    2

--- algoritmo.js
+++ algoritmo.js
@@ -2,4 +2,10 @@ window.onload = function() {
 	//0 - Vetores com as informacoes de cores e nomes de regioes a serem utilizadas no mapa e na legenda
 	var cores = ["#7fc97f", "#beaed4", "#fdc086", "#ffff99", "#386cb0"];
 	var regioes = ["Norte", "Nordeste", "Sudeste", "Sul", "Centro Oeste"];
+	
+	//2 - Camadas base
+	var osmPretoBranco = L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
+		attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+	}).addTo(map);
+	var osmColorido = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png");
 }
