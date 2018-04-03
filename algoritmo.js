9aeee7b7466c40ac8c67122f3844ff88f76f1c71
Eduardo Silverio da Silva <eduardosilverio1990@gmail.com>
2018-04-02 22:20:16 -0400

    bugs

--- algoritmo.js
+++ algoritmo.js
@@ -74,7 +74,7 @@ window.onload = function() {
   //11 - Evento de clique (abrir popup que apresenta as coordenadas)
 	map.on("click", function(evento) {
 		L.popup().setLatLng(evento.latlng).setContent("Latitude: " + evento.latlng.lat.toFixed(2) + " Longitude: " + evento.latlng.lng.toFixed(2)).openOn(map);
-  }
+    });
          
   //12 - Adicionar posiçao do usuário ao mapa
 	map.locate();	
